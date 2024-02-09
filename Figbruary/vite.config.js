e722235190e905b223f77d507288cd19d6b78ff9
jonathancdays <jonathancdays@gmail.com>
2024-02-09 11:43:12 -0500

    Change routing again again

---
+++ Figbruary/vite.config.js
@@ -0,0 +1,16 @@
+// vite.config.js
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about/index.html'),
+        contact: resolve(__dirname, 'contact/index.html'),
+        gallery: resolve(__dirname, 'gallery/index.html'),
+      },
+    },
+  },
+})
